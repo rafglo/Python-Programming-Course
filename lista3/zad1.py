cb342dd5dd9499945dc3f17e307650851e0755e0
rafglo <127320711+rafglo@users.noreply.github.com>
2023-04-17 16:43:19 +0200

    Update zad1.py

--- lista3/zad1.py
+++ lista3/zad1.py
@@ -1,11 +1,23 @@
 import zipfile, os, datetime, time
 from pathlib import Path
 
-def cody_mod(folders, mod_days, out_path, extension):
+def copy_mod(folders, out_path, extension, mod_time = 3, mod_format = "d"):
+    mod_formats = ["s", "m", "h", "d", "w", "y"]
+    mod_index = mod_formats.index(mod_format)
+    mod_div = [1, 60, 3600, 3600*24, 3600*24*7, 3600*24*365]
     date = str(datetime.date.today())
     zip_name = "copy - " + date + ".zip"
     if_exists = os.path.exists(out_path + "\\Backup")
     if if_exists:
+        names = []
+        for root, dirs, files in os.walk(out_path + "\\Backup"):
+            for file in files:
+                names.append(os.path.basename(os.path.join(root, file))[1:])
+        how_much = names.count(zip_name)
+        if how_much > 0:
+            zip_name = str(how_much + 1) + "copy - " + date + ".zip"
+        else:
+            zip_name = "1" + "copy - " + date + ".zip"
         zip_path = out_path + "\\Backup\\" + zip_name
     else:
         backup_path = out_path + "\\Backup\\"
@@ -17,10 +29,10 @@ def cody_mod(folders, mod_days, out_path, extension):
             for root, dirs, files in os.walk(folder):
                 for file in files:
                     mod = os.path.getmtime(os.path.join(root, file))
-                    delta = (time.time() - mod) / (3600 * 24)
-                    if Path(file).suffix == extension and delta < int(mod_days):
-                        zf.write(os.path.join(root, file), os.path.basename(os.path.join(root, file)))
+                    delta = (time.time() - mod) / mod_div[mod_index]
+                    if Path(file).suffix == extension and delta < mod_time:
+                        zf.write(os.path.join(root, file))
     zf.close()
 
 
-cody_mod([r"C:\Users\Rafal\OneDrive\Pulpit\goegrafia", r"C:\Users\Rafal\OneDrive\Pulpit\informatyka"], 3, r"C:\Users\Rafal\OneDrive\Pulpit", ".txt")
+copy_mod([r"C:\Users\Rafal\OneDrive\Pulpit\goegrafia", r"C:\Users\Rafal\OneDrive\Pulpit\informatyka"], r"C:\Users\Rafal\OneDrive\Pulpit", ".txt", 3, "w")
